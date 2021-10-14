66fcf2f86055365e4a5f16d4b7c775bf6d9c5e07
TerrenceChao <rtyufjvbn@gmail.com>
2021-10-14 12:35:09 +0800

    fix: 重新命名 sharding algorithm 中的參數

--- src/main/java/com/example/todolist/db/rmdb/sharding/algorithm/BaseHashcodeAlgorithm.java
+++ src/main/java/com/example/todolist/db/rmdb/sharding/algorithm/BaseHashcodeAlgorithm.java
@@ -12,18 +12,18 @@ public abstract class BaseHashcodeAlgorithm implements PreciseShardingAlgorithm<
     protected abstract int shardByHashcode(String hashcode);
 
     /**
-     * @param databaseNames 所有分片庫的集合
+     * @param collection databaseNames or tableNames >> 所有分庫 or 分片的集合
      * @param shardingValue 為分片屬性，其中 logicTableName 為邏輯表，columnName 分片健（字段），value 為 SQL 中解析出的分片健的值
      * @return
      */
     @Override
-    public String doSharding(Collection<String> databaseNames, PreciseShardingValue<String> shardingValue) {
+    public String doSharding(Collection<String> collection, PreciseShardingValue<String> shardingValue) {
 
-        int dbAmount = databaseNames.size();
-        String value = shardByHashcode(shardingValue.getValue()) % dbAmount + 1 + "";
-        for (String databaseName : databaseNames) {
-            if (databaseName.endsWith(value)) {
-                return databaseName;
+        int size = collection.size();
+        String value = shardByHashcode(shardingValue.getValue()) % size + 1 + "";
+        for (String shard : collection) {
+            if (shard.endsWith(value)) {
+                return shard;
             }
         }
 
